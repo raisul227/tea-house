a049399b7142981a660eefa59d1e1b1161283b72
raisul227 <raisul2277@gmail.com>
2025-07-10 09:37:35 +0600

    simple landing page initial upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Manrope', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
